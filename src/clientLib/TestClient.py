cecbdf95f53dd99d159d075cd087940f14384625
Collin Kees <Collin@kees.net>
2023-11-01 10:49:51 -0700

    rename client

--- src/clientLib/TestClient.py
+++ src/clientLib/TestClient.py
@@ -1,30 +1,35 @@
 from ZTRequests import ZTRequests
+import requests
 
 
 # Edge node server URL TODO don't hard code this
 edge_node_url = "http://127.0.0.1:5000"  # Replace with the actual address and port
+# TODO compare the requests routed through edge nodes to direct requests
+backend_server_url = "http://127.0.0.1:5002" # Replace with the actual address and port
 
 # Function to send a request to the edge node
 def TestRequests(data):
     try:
         print("Posting...")
-        response = ZTRequests.post(f"{edge_node_url}/arandomroute", json=data)
+        response = ZTRequests.post(f"{edge_node_url}/testPost", json=data)
+        response2 = requests.post(f"{backend_server_url}/testPost", json=data)
         checkResponse(response)
+        checkResponseSimilarity(response, response2)
 
         print("Geting...")
-        response = ZTRequests.get(f"{edge_node_url}/arandomroute")
+        response = ZTRequests.get(f"{edge_node_url}/testGet")
         checkResponse(response)
 
         print("Puting...")
-        response = ZTRequests.put(f"{edge_node_url}/arandomroute", json=data)
+        response = ZTRequests.put(f"{edge_node_url}/testPut", json=data)
         checkResponse(response)
 
         print("Deleting...")
-        response = ZTRequests.delete(f"{edge_node_url}/arandomroute")
+        response = ZTRequests.delete(f"{edge_node_url}/testDelete")
         checkResponse(response)
 
         print("Heading...")
-        response = ZTRequests.head(f"{edge_node_url}/arandomroute")
+        response = ZTRequests.head(f"{edge_node_url}/testHead")
         checkResponse(response)
 
         print("\nDone")
@@ -35,8 +40,19 @@ def TestRequests(data):
 def checkResponse(response):
     if response.status_code == 200:
             print("Request successfully sent to edge node")
+
+            # print data if there is any
+            if response.text:
+                print(response.text)
     else:
         print("Request failed to send to edge node with status code:", response.status_code)
+
+def checkResponseSimilarity(r1, r2):
+    if r1.status_code == r2.status_code:
+        if r1.text == r2.text:
+            print("Responses are the same")
+            return 
+    print("Responses are different")
 
 if __name__ == "__main__":
     # dummy data to send
