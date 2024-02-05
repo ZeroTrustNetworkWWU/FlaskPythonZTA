29b97c7661209014f465acfe1b7898424b66f824
Collin Kees <Collin@kees.net>
2024-02-05 12:53:57 -0800

    Refactor start script and remove initVenv.bat

--- StartServers.bat
+++ StartServers.bat
@@ -1,5 +1,31 @@
-call venv\Scripts\activate
-start venv\Scripts\python.exe src\edgeNode\EdgeNode.py
-start venv\Scripts\python.exe src\trustEngine\TrustEngine.py
-start venv\Scripts\python.exe testing\TestServer.py
-start cmd /k venv\Scripts\python.exe src\clientLib\TestClient.py
+@echo off
+
+@echo off
+
+REM Check if venv exists
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+    call venv\Scripts\activate.bat
+    pip install -r requirements.txt
+) else (
+    REM Activate existing venv
+    call venv\Scripts\activate.bat
+)
+
+
+REM Start Backend server
+start cmd /k venv\Scripts\python.exe src\testing\TestServer.py
+
+REM Start EdgeNode server
+cd "EdgeNode"
+cmd /c start.bat
+
+REM Start TrustEngine server
+cd "../TrustEngine"
+cmd /c start.bat
+
+REM Start TestClient
+cd "../PythonAPI"
+cmd /c start.bat
+
