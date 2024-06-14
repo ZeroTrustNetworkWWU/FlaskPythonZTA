dce31ed78f84fafa433000b0f06dcba68c15b934
Asher Noland <nolanda@wwu.edu>
2024-06-14 00:46:47 -0700

    Completed API for this quarter & accompanying tests
    see Trust Engine submodule for more details

--- src/testing/Trying.py
+++ src/testing/Trying.py
@@ -1,11 +1,15 @@
 import requests
+import json
 
 url = 'http://127.0.0.1:5001'
 
 # Trying getRoles
 print("\nTesting roles gathering...")
 response = requests.get(url+'/getRoles')
-print(response.content)
+print(response.text)
+
+# Save to revert changes later
+old_roles = json.loads(response.text)
 
 # Trying addRole
 print("\nTesting role adding...")
@@ -17,12 +21,15 @@ data = {
     }
 }
 response = requests.post(url+'/addRole', json=data)
-print(response.content)
+print(response.text)
 
 # Confirmation
 print("\nConfirming role addition...")
 response = requests.get(url+'/getRoles')
-print(response.content)
+print(response.text)
+
+# Save for future test
+withtestrole = json.loads(response.text)
 
 # Trying removeRole
 print("\nTesting role removal...")
@@ -34,17 +41,20 @@ data = {
     }
 }
 response = requests.put(url+'/removeRole', json=data)
-print(response.content)
+print(response.text)
 
 # Confirmation
 print("\nConfirming role deletion...")
 response = requests.get(url+'/getRoles')
-print(response.content)
+print(response.text)
 
 # Trying getUsers
 print("\nTesting users gathering...")
 response = requests.get(url+'/getUsers')
-print(response.content)
+print(response.text)
+
+# Save to revert changes later
+old_users = json.loads(response.text)
 
 # Trying addUser
 print("\nTesting user adding, using already implemented user registration...")
@@ -53,7 +63,15 @@ data = {
     "password": "testPassword"
 }
 response = requests.post(url+'/addUser', json=data)
-print(response.content)
+print(response.text)
+
+# Confirmation
+print("\nConfirming user addition...")
+response = requests.get(url+'/getUsers')
+print(response.text)
+
+# Save for future test
+withtestuser = json.loads(response.text)
 
 # Trying removeUser
 print("\nTesting user removal...")
@@ -61,5 +79,33 @@ data = {
     "user": "testUser"
 }
 response = requests.put(url+'/removeUser', json=data)
-print(response.content)
+print(response.text)
 
+# Confirmation
+print("\nConfirming user deletion...")
+response = requests.get(url+'/getUsers')
+print(response.text)
+
+# Trying updateUserList
+print("\nTesting user list updating...")
+print("unchanged original:")
+response = requests.post(url+'/updateUserList', json=old_users)
+print(response.text)
+print("return of testUser:")
+response = requests.post(url+'/updateUserList', json=withtestuser)
+print(response.text)
+print("back to original:")
+response = requests.post(url+'/updateUserList', json=old_users)
+print(response.text)
+
+# Trying updateRoleList
+print("\nTesting role list updating...")
+print("unchanged original:")
+response = requests.post(url+'/updateRoleList', json=old_roles)
+print(response.text)
+print("return of testRole:")
+response = requests.post(url+'/updateRoleList', json=withtestrole)
+print(response.text)
+print("back to original:")
+response = requests.post(url+'/updateRoleList', json=old_roles)
+print(response.text)
